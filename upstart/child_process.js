9e54b7c36a113fb8a8b0a58f61628cc8f3c0bcf9
Ge Yang <yangge1987@gmail.com>
2014-03-05 09:36:39 -0700

    added files from http://blog.argteam.com/coding/hardening-nodejs-production-process-supervisor/

--- upstart/child_process.js
+++ upstart/child_process.js
@@ -1,47 +1,51 @@
-var child_process, delayTimeout, healthCheckInterval, _;
+'user strict';
 
-_ = require('underscore')._;
+var _ = require('underscore')._,
+    child_process = require('child_process');
 
-child_process = require('child_process');
+var healthCheckInterval = 60 * 1000;
 
-healthCheckInterval = 60 * 1000;
-
-delayTimeout = function(ms, func) {
-  return setTimeout(func, ms);
+function delayTimeout(ms, func) {
+    return setTimeout(func, ms);
 };
 
-exports.spawnMonitoredChild = function(script, port, healthCheck, environmentVariables) {
-  var respawn;
-  respawn = function() {
-    var child, delayedHealthCheck, healthCheckTimeout;
-    child = child_process.spawn(process.execPath, [script], {
-      env: _.extend(environmentVariables, process.env)
-    });
-    console.log("Started child, port=" + port + ", pid=" + child.pid);
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-    healthCheckTimeout = null;
-    delayedHealthCheck = function() {
-      return healthCheckTimeout = delayTimeout(healthCheckInterval, function() {
-        var start;
-        start = new Date();
-        return healthCheck(port, function(healthy) {
-          if (healthy) {
-            console.log("" + port + " is healthy - ping time " + (new Date() - start) + "ms");
-            return delayedHealthCheck();
-          } else {
-            console.error("" + port + " did not respond in time - killing it");
-            return child.kill();
-          }
+exports.spawnMonitoredChild = function (script, port, healthCheck, environmentVariables) {
+    function respawn() {
+
+        var child = child_process.spawn(
+            process.execPath,
+            [script],
+            { env: _.extend(environmentVariables, process.env) }
+        );
+
+        console.log("Started child, port=" + port + ", pid=" + child.pid);
+
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+        var healthCheckTimeout = null;
+
+        function delayedHealthCheck () {
+            return healthCheckTimeout = delayTimeout(healthCheckInterval, function () {
+                var start;
+                start = new Date();
+                return healthCheck(port, function (healthy) {
+                    if (healthy) {
+                        console.log("" + port + " is healthy - ping time " + (new Date() - start) + "ms");
+                        return delayedHealthCheck();
+                    } else {
+                        console.error("" + port + " did not respond in time - killing it");
+                        return child.kill();
+                    }
+                });
+            });
+        };
+
+        child.on('exit', function (code, signal) {
+            clearTimeout(healthCheckTimeout);
+            console.error("Child exited with code " + code + ", signal " + signal + ", respawning");
+            return respawn();
         });
-      });
+        return delayedHealthCheck();
     };
-    child.on('exit', function(code, signal) {
-      clearTimeout(healthCheckTimeout);
-      console.error("Child exited with code " + code + ", signal " + signal + ", respawning");
-      return respawn();
-    });
-    return delayedHealthCheck();
-  };
-  return respawn();
+    return respawn();
 };
