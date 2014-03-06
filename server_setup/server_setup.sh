1531b885075891be694aebffd6881a5d40f6bbe7
Ge Yang <yangge1987@gmail.com>
2014-03-06 00:36:12 -0600

    server_setup script

---
+++ server_setup/server_setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install rcconf
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
+sudo apt-get install git-core
+sudo apt-get install nodejs
+
