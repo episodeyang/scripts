83e33509a95524f697adb2157922aec6c69cc0aa
Ge Yang <yangge1987@gmail.com>
2014-03-08 12:24:46 -0800

    added chown

--- server_setup/install_nodejs_npm.sh
+++ server_setup/install_nodejs_npm.sh
@@ -5,5 +5,6 @@ sudo apt-get update
 sudo apt-get install nodejs
 node -v
 npm -v
-
+# change ownership of folder to avoild using sudo for npm install
 sudo chown -R `whoami` ~/.npm
+sudo chown -R `whoami` ~
