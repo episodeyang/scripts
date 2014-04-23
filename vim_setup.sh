63a98fcc6f8f590fc84350e4408aeaa72fb2241c
Ge Yang <yangge1987@gmail.com>
2014-04-23 17:26:09 -0500

    update vim setup file

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,1 +1,8 @@
 cp .vimrc.default ~/.vimrc
+mkdir ~/.vim
+mkdir ~/.vim/syntax
+cp vimFiles/python.vim ~/.vim/syntax/
+mkdir ~/.vim/colors
+cp vimFiles/blackboard.vim ~/.vim/colors/
+cp vimFiles/zenburn.vim ~/.vim/colors/
+cp vimFiles/codeschool.vim ~/.vim/colors/
