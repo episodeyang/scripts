52be05410fac56ee036f6eab059309ed516280c4
Ge Yang <yangge1987@gmail.com>
2014-04-23 17:47:48 -0500

    add nice folding text script to .vimrc.defaut

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,7 +1,13 @@
 cp .vimrc.default ~/.vimrc
+
 mkdir ~/.vim
 mkdir ~/.vim/syntax
 cp vimFiles/python.vim ~/.vim/syntax/
+
+mkdir ~/.vim/ftplugin
+mkdir ~/.vim/ftplugin/python
+cp vimFiles/SimpylFold.vim ~/.vim/ftplugin/python
+
 mkdir ~/.vim/colors
 cp vimFiles/blackboard.vim ~/.vim/colors/
 cp vimFiles/zenburn.vim ~/.vim/colors/
