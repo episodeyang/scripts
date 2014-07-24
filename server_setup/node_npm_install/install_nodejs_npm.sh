c784491433cb9b980f17590099c949e535a9a16e
Ge Yang <yangge1987@gmail.com>
2014-07-24 16:11:28 +0000

    cleanup and organization

---
+++ server_setup/node_npm_install/install_nodejs_npm.sh
@@ -0,0 +1,13 @@
+sudo apt-get install python-software-properties
+sudo apt-add-repository ppa:chris-lea/node.js
+sudo apt-get update
+
+sudo apt-get install nodejs
+node -v
+npm -v
+# change ownership of folder to avoild using sudo for npm install
+# might have to run *after* folder are made.
+sudo chown -R `whoami` ~/.npm
+sudo chown -R `whoami` ~
+sudo chown -R `whoami` /usr/bin/node
+sudo chown -R `whoami` /usr/bin/npm
